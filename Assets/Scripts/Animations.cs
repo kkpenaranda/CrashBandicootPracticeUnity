d2fd1e88b79798936f3b31ac3031c3f64dbd89f6
kkpenaranda <kk.penaranda@uniandes.edu.co>
2022-04-04 15:56:19 -0500

    Design of first and second room in lobby

---
+++ Assets/Scripts/Animations.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    public string animName;
+    public string animNameExit;
+    public string gameObjectName;
+    public int quantity;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="PlayerTrigger")
+        {
+            for (int i=0; i<quantity;i++)
+            {
+                GameObject.Find(gameObjectName + "_"+ i.ToString()).GetComponent<Animator>().SetTrigger(animName);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerTrigger")
+        {
+            for (int i = 0; i < quantity; i++)
+            {
+                GameObject.Find(gameObjectName + "_" + i.ToString()).GetComponent<Animator>().SetTrigger(animNameExit);
+            }
+        }
+    }
+}
